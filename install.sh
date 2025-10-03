1002760499a588638e8afa9818abd6f6af366a06
Dalhats L. Avicena <140884684+WeakestLinuxEnjoyer@users.noreply.github.com>
2025-10-03 10:42:04 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-# Requirements : Ollama, Python3, PIP
-
-# Installing the required model
-ollama pull hf.co/mecha-org/linux-command-generator-llama3.2-1b:F16
-
-# Installing the required Python module
-pip install ollama
